977ea3474f0c6520310f6204315864682ae5dda5
wilson Neto <wngaspar10@gmail.com>
2023-02-01 11:56:33 -0300

    feat: create component Home and Pokemons

---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <main id="main">
+    <h1>Olá, Seja bem vindo ao seu sistema de Pokéteam!</h1>
+    <section class="section">
+      <a class="link" rel="pokemons" @click="toPokemons()">
+        <div class="item">
+          Pokémons
+        </div>
+      </a>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  app: "Home",
+  methods: {
+    toPokemons() {
+      this.$router.push("/pokemons");
+    }
+  }
+}
+</script>
+
+<style>
+#main {
+  text-align: center;
+  margin-top: 60px;
+}
+.section {
+  max-width: 800px;
+  height: 300px;
+  margin: 80px auto 0 auto;
+  display: flex; 
+  align-items: center;
+  justify-content: space-around;
+  background-color: currentColor;
+}
+.link {
+  text-decoration: none;
+  height: 200px;
+  width: 200px;
+  padding: 10px;
+  text-transform: uppercase;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: #ccc;
+}
+.item{
+  color: #2c3e50;
+  margin-top: 43%;
+}
+</style>
---
+++ src/components/Pokemons.vue
@@ -0,0 +1,45 @@
+<template>
+  <main id="main">
+    <h1>Pokemons!</h1>
+    <router-link to="/">Home</router-link>
+    
+  </main>
+</template>
+
+<script>
+export default {
+  app: "Home",
+  methods: {}
+}
+</script>
+
+<style>
+#main {
+  text-align: center;
+  margin-top: 60px;
+}
+.section {
+  max-width: 800px;
+  height: 300px;
+  margin: 80px auto 0 auto;
+  display: flex; 
+  align-items: center;
+  justify-content: space-around;
+  background-color: currentColor;
+}
+.link {
+  text-decoration: none;
+  height: 200px;
+  width: 200px;
+  padding: 10px;
+  text-transform: uppercase;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: #ccc;
+}
+.item{
+  color: #2c3e50;
+  margin-top: 43%;
+}
+</style>
