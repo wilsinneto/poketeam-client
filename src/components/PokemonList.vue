f3027a030b9030e352113acff4c548252b73f5f9
wilson Neto <wngaspar10@gmail.com>
2023-02-04 16:09:06 -0300

    feat: create pokemon list component

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <ul class="list-group">
+        <div class="card-group">
+          <div class="card d-flex flex-row">
+            <li class="list-group-item" v-for="pokemon in pokemons" :key="pokemon.id">
+              <img :src="pokemon.imageUrl" class="card-img-top" :alt="pokemon.name">
+              <div class="card-body">
+                <h5 class="card-title">{{ pokemon.name }}</h5>
+                <span class="text-primary">{{ pokemon.specie }}</span>
+              </div>
+            </li>
+          </div>
+        </div>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokemonList",
+  props: ["pokemons"],
+}
+</script>
+
+<style>
+
+</style>
