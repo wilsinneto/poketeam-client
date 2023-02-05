b9d91a6232eb7737ecb402b18b769553eda0bbae
wilson Neto <wngaspar10@gmail.com>
2023-02-05 09:51:35 -0300

    feat: added pokemon view details route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 import Home from "@/components/Home.vue";
 import Pokemons from '@/components/Pokemons.vue';
 import PokemonTeam from '@/components/PokemonTeam.vue';
+import PokemonViewDetails from '@/components/PokemonViewDetails.vue';
 
 export default [
   { path: '/', component: Home },
   { path: '/pokemons', component: Pokemons },
   { path: '/pokemon-team', component: PokemonTeam },
+  { path: '/pokemon-view-details', component: PokemonViewDetails},
   { path: "*", redirect: "/" }
 ]
