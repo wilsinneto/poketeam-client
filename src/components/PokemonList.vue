9fa9a456ec74ba97741f273f012a482a726fe1b9
wilson Neto <wngaspar10@gmail.com>
2023-02-05 09:50:36 -0300

    style: added cursor pointer

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -5,10 +5,12 @@
         <div class="card-group">
           <div class="card d-flex flex-row">
             <li class="list-group-item" v-for="pokemon in pokemons" :key="pokemon.id">
-              <img :src="pokemon.imageUrl" class="card-img-top" :alt="pokemon.name">
-              <div class="card-body">
-                <h5 class="card-title">{{ pokemon.name }}</h5>
-                <span class="text-primary">{{ pokemon.specie }}</span>
+              <div style="cursor: pointer;" @click="openPokemonViewDetail(pokemon)">
+                <img :src="pokemon.imageUrl" class="card-img-top" :alt="pokemon.name">
+                <div class="card-body">
+                    <h5 class="card-title">{{ pokemon.name }}</h5>
+                    <span class="text-primary">{{ pokemon.specie }}</span>
+                </div>
               </div>
             </li>
           </div>
@@ -22,6 +24,11 @@
 export default {
   name: "PokemonList",
   props: ["pokemons"],
+  methods: {
+    openPokemonViewDetail(pokemon) {
+      this.$router.push({ path: "/pokemon-view-details", query: { data: pokemon } })
+    }
+  }
 }
 </script>
 
